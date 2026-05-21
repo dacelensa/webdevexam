821e393a6a89d71b9870286cfcdc3346767d9b41
dacelensa <dacelensa00@gmail.com>
2026-05-21 18:47:57 +0200

    Added javascript file

---
+++ static/functions.js
@@ -0,0 +1,6 @@
+function open_mobile_menu() {
+    document.getElementById("mobile-menu").classList.add("visible")
+}
+function close_mobile_menu() {
+    document.getElementById("mobile-menu").classList.remove("visible")
+}
