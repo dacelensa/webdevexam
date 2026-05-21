ad04918317835b681405ee47c7317b84d084e691
dacelensa <dacelensa00@gmail.com>
2026-05-21 18:46:25 +0200

    Added vendor specific offers

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ from flask import Flask, render_template
 import json
 from datetime import datetime
 
-with open("data/produce.json", "r", encoding="utf8") as f:
-    all_produce = json.load(f)
 
-with open("data/vendors.json", "r", encoding="utf8") as f:
-    all_vendors = json.load(f)
-
-with open("data/offers.json", "r", encoding="utf8") as f:
-    all_offers = json.load(f)
-
-app = Flask(__name__)
+def process_offer_data(data_to_process):
+    offer_data = []
+    for offer in data_to_process:
+        for vendor in all_vendors:
+            if vendor["id"] == offer["vendor_id"]:
+                offer["vendor_name"] = vendor["name"]
+                offer["vendor_address"] = vendor["address"]
+                offer["vendor_image"] = vendor["image"]
+        for produce in all_produce:
+            if produce["id"] == offer["produce_id"]:
+                offer["produce_name"] = produce["name"]
+                offer["produce_image"] = produce["image"]
+        offer_data.append(offer)
+    return offer_data
 
 
 def get_season_name():
@@ -26,6 +31,19 @@ def get_season_name():
         return "winter"
 
 
+with open("data/produce.json", "r", encoding="utf8") as f:
+    all_produce = json.load(f)
+
+with open("data/vendors.json", "r", encoding="utf8") as f:
+    all_vendors = json.load(f)
+
+with open("data/offers.json", "r", encoding="utf8") as f:
+    all_loaded_offers = json.load(f)
+    all_offers = process_offer_data(all_loaded_offers)
+
+app = Flask(__name__)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -33,7 +51,19 @@ def index():
 
 @app.route("/vendors")
 def vendors():
-    return render_template("vendors.html", list_of_vendors=all_vendors)
+    updated_vendors = []
+
+    for original_vendor in all_vendors.copy():
+        vendor = original_vendor.copy()
+        vendor["produce_names"] = []
+
+        for offer in all_offers:
+            if offer["vendor_id"] == vendor["id"]:
+                vendor["produce_names"].append(offer["produce_name"])
+
+        updated_vendors.append(vendor)
+
+    return render_template("vendors.html", list_of_vendors=updated_vendors)
 
 
 @app.route("/produce")
@@ -41,7 +71,8 @@ def produce():
     in_season = []
     out_of_season = []
     current_season = get_season_name()
-    for produce in all_produce.copy():
+    for original_produce in all_produce.copy():
+        produce = original_produce.copy()
         if current_season in produce["season"]:
             produce["in_season"] = True
             in_season.append(produce)
@@ -59,13 +90,9 @@ def individual_produce(produce_id):
         if i["id"] == int(produce_id):
             produce = i
 
-    for offer in all_offers.copy():
+    for original_offer in all_offers.copy():
+        offer = original_offer.copy()
         if offer["produce_id"] == int(produce_id):
-            for vendor in all_vendors:
-                if vendor["id"] == offer["vendor_id"]:
-                    offer["vendor_name"] = vendor["name"]
-                    offer["vendor_address"] = vendor["address"]
-                    offer["vendor_image"] = vendor["image"]
             offers_for_produce.append(offer)
 
     return render_template(
