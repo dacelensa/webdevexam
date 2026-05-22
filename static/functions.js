8d77f05a49cd036bf098c4721d7fbbdc100c7c10
dacelensa <dacelensa00@gmail.com>
2026-05-22 13:31:39 +0200

    Added js fiilter for in stock items only

--- static/functions.js
+++ static/functions.js
@@ -4,3 +4,15 @@ function open_mobile_menu() {
 function close_mobile_menu() {
     document.getElementById("mobile-menu").classList.remove("visible")
 }
+function filter_in_stock_produce() {
+    var is_checked = document.getElementById("show-only-in-stock").checked
+    var offers_out_of_stock = document.querySelectorAll(".out-of-stock")
+    for (var offer of offers_out_of_stock) {
+        if (is_checked) {
+            offer.classList.add("hidden")
+        }
+        else {
+            offer.classList.remove("hidden")
+        }
+    }
+}
